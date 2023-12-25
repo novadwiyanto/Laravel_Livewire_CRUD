ef3e6763bcb5fcb9572e95e5a770a8894ac080d7
novadwiyanto <novadwynt28@gmail.com>
2023-12-25 14:04:32 +0700

    1.0 Done

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,20 +1,35 @@
 <div class="p-3">
-  <div class="card p-3">
-  <form wire:submit="add">
-    <div class="form-group">
-      <label for="name" >Name</label>
-      <input type="text" class="form-control mx-auto m-2" id="name" wire:model="form.name">  <div>
-        @error('form.name') <span class="error">{{ $message }}</span> @enderror 
-      </div>
+    <div class="card p-3">
+        @if ($update_data == true)
+            <form wire:submit="update">
+            @else
+                <form wire:submit="add">
+        @endif
+        @csrf
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control mx-auto m-2" id="name" wire:model="form.name">
+            <div>
+                @error('form.name')
+                    <span class="error">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <input type="text" class="form-control mx-auto m-2" id="description" wire:model="form.description">
+            <div>
+                @error('form.description')
+                    <span class="error">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+        @if ($update_data == true)
+            <button type="sumbit" class="btn btn-primary mx-auto m-2">Update</button>
+            <button class="btn btn-danger mx-auto m-2" wire:click="$refresh">Cancel</button>
+        @else
+            <button type="submit" class="btn btn-primary mx-auto m-2">Add</button>
+        @endif
+        </form>
     </div>
-    <div class="form-group">
-      <label for="description">Description</label>
-      <input type="text" class="form-control mx-auto m-2" id="description" wire:model="form.description">
-      <div>
-        @error('form.description') <span class="error">{{ $message }}</span> @enderror 
-      </div>
-    </div>
-    <button type="submit" class="btn btn-primary mx-auto m-2">Submit</button>
-  </form>
-</div>
 </div>
--- resources/views/livewire/table-post.blade.php
+++ resources/views/livewire/table-post.blade.php
@@ -1,26 +1,29 @@
 <div class="mt-3">
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">No</th>
-        <th scope="col">Post</th>
-        <th scope="col">Description</th>
-        <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-    @foreach ($post as $key => $item)
-          <div> 
-              <tr>
-                <th>{{ $post->firstItem() + $key}}</th>
-                <td>{{ $item->name }}</td>
-                <td>{{ $item->description }}</td>
-                <td><button class="btn btn-danger" wire:click="delete({{ $item->id }})">Delete</button></td>
-              </tr>
-          </div>
-      @endforeach
-    </tbody>
-  </table>
-  {{ $post->links() }}
-  
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Post</th>
+                <th scope="col">Description</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($post as $key => $item)
+                <div>
+                    <tr>
+                        <th>{{ $post->firstItem() + $key }}</th>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->description }}</td>
+                        <td>
+                            <button class="btn btn-info" wire:click="update({{ $item->id }})">Update</button>
+                            <button class="btn btn-danger" wire:click="delete({{ $item->id }})">Delete</button>
+                        </td>
+                    </tr>
+                </div>
+            @endforeach
+        </tbody>
+    </table>
+    {{ $post->links() }}
+
 </div>
